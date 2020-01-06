ac0659d9503f23c20076b0ff6a039c70abaa755c
Justin Miller <jayman1294@gmail.com>
2020-01-06 07:05:34 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,26 +9,3 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
   
   
-  
-  
-  
-  def up 
-  end
-  
-  def down
-  end
-end
-
-
-
-sql = <<-SQL 
-CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-)
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
